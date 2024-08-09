77cd3cb7cdcea0544270dd4f4807932e87e6a18f
sebastian-nunez <02snunez@gmail.com>
2024-08-09 19:23:25 -0400

    feat: adds the cart service

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -7,6 +7,8 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/sebastian-nunez/golang-store-api/service/auth"
+	"github.com/sebastian-nunez/golang-store-api/service/cart"
+	"github.com/sebastian-nunez/golang-store-api/service/order"
 	"github.com/sebastian-nunez/golang-store-api/service/product"
 	"github.com/sebastian-nunez/golang-store-api/service/user"
 )
@@ -41,6 +43,11 @@ func (s *Server) Run() error {
 	productStore := product.NewStore(s.db)
 	productHandler := product.NewHandler(productStore)
 	productHandler.RegisterRoutes(subrouter)
+
+	// Cart/Orders
+	orderStore := order.NewStore(s.db)
+	cartHandler := cart.NewHandler(productStore, orderStore, userStore)
+	cartHandler.RegisterRoutes(subrouter)
 
 	log.Println("Server: listening on port", s.addr)
 	return http.ListenAndServe(s.addr, router)
---
+++ service/cart/routes.go
@@ -0,0 +1,73 @@
+package cart
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/gorilla/mux"
+	"github.com/sebastian-nunez/golang-store-api/service/auth"
+	"github.com/sebastian-nunez/golang-store-api/types"
+	"github.com/sebastian-nunez/golang-store-api/utils"
+)
+
+type Handler struct {
+	store      types.ProductStore
+	orderStore types.OrderStore
+	userStore  types.UserStore
+}
+
+func NewHandler(
+	store types.ProductStore,
+	orderStore types.OrderStore,
+	userStore types.UserStore,
+) *Handler {
+	return &Handler{
+		store:      store,
+		orderStore: orderStore,
+		userStore:  userStore,
+	}
+}
+
+func (h *Handler) RegisterRoutes(router *mux.Router) {
+	router.HandleFunc("/cart/checkout", h.handleCheckout).Methods(http.MethodPost)
+}
+
+func (h *Handler) handleCheckout(w http.ResponseWriter, r *http.Request) {
+	userID := auth.GetUserIDFromContext(r.Context())
+
+	var cart types.CartCheckoutRequest
+	if err := utils.ParseJson(r, &cart); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	if err := utils.Validate.Struct(cart); err != nil {
+		errors := err.(validator.ValidationErrors)
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("invalid payload: %v", errors))
+		return
+	}
+
+	productIds, err := getCartItemsIDs(cart.Items)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	products, err := h.store.GetProductsByID(productIds)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	orderID, totalPrice, err := h.createOrder(products, cart.Items, userID)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	utils.WriteJson(w, http.StatusOK, map[string]any{
+		"totalPrice": totalPrice,
+		"orderId":    orderID,
+	})
+}
